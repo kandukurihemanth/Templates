c57575fbdfd20b2674fd054fde2b542ee9031aff
kandukurihemanth <105703779+kandukurihemanth@users.noreply.github.com>
2022-09-15 15:24:55 +0530

    Update variables.tf

--- ec2_4.1.4/variables.tf
+++ ec2_4.1.4/variables.tf
@@ -173,7 +173,7 @@ variable "tenancy" {
 variable "user_data" {
   description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
   type        = string
-  default     = null
+  
 }
 
 variable "user_data_base64" {
